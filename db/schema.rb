66bffa8fc23bec096b0b0ac208936ebba7b308a0
Antoine de Broqueville <antoinedebroqueville@gmail.com>
2020-03-18 10:51:01 +0100

    city added to nannies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_145338) do
+ActiveRecord::Schema.define(version: 2020_03_18_094939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_145338) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
     t.index ["user_id"], name: "index_nannies_on_user_id"
   end
 

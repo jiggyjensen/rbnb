85df12632b43d273229c5efa3c86eee7edf3ef4d
Maxime Geurts <maxime.geurts@hotmail.com>
2020-03-19 17:37:35 +0100

    dashboard and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   resources :nannies, only: [:new, :create, :index, :show] do
     resources :bookings, only: [:new, :create]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :bookings, only: [:index] do
+    member do
+      patch '/accept', to: 'bookings#accept'
+      patch '/reject', to: 'bookings#reject'
+    end
+  end # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

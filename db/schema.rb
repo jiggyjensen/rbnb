fde6cbe17ac5fddef3e7aad926bc7cd3e07ef453
Jensen Daniel <jadd3791@gmail.com>
2020-03-19 17:36:15 +0100

    added rating to nannies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_104821) do
+ActiveRecord::Schema.define(version: 2020_03_19_104939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_104821) do
     t.string "city"
     t.float "latitude"
     t.float "longitude"
+    t.integer "rating"
     t.index ["user_id"], name: "index_nannies_on_user_id"
   end
 
